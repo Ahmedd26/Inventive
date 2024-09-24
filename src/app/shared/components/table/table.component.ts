24af8a15064ce2299e1502e17dc9acb5bfd8e03f
Ahmedd26 <ahmedabdellatife1@gmail.com>
2024-09-24 20:00:58 +0300

    added context menu UI, code improvments

--- src/app/shared/components/table/table.component.ts
+++ src/app/shared/components/table/table.component.ts
@@ -1,6 +1,14 @@
-import { Component, Input, SimpleChanges, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  SimpleChanges,
+  OnInit,
+  AfterViewInit,
+  OnChanges,
+} from '@angular/core';
 import { IconsModule } from '../../icons/icons.module';
 import { CommonModule } from '@angular/common';
+import { initFlowbite } from 'flowbite';
 
 interface ITable {
   id: number | string;
@@ -13,10 +21,10 @@ interface ITable {
   imports: [IconsModule, CommonModule],
   templateUrl: './table.component.html',
 })
-export class TableComponent implements OnInit {
+export class TableComponent implements OnInit, AfterViewInit, OnChanges {
   @Input({ required: true }) tableData: ITable[] = [];
   @Input({ required: true }) tableHeaders: string[] = [];
-  @Input() limit: number = 10;
+  @Input() limit: number = 5;
   currentPage = 1;
   total: number = 0;
   start = 0;
@@ -24,16 +32,71 @@ export class TableComponent implements OnInit {
   pages: number[] = [];
   pagesCount = 1;
 
-  ngOnInit(): void {
-    this.updatePages();
-  }
-
   // Pagination
   changePage(page: number): void {
     this.currentPage = page;
     this.updatePages();
+    this.updateDisplayRange();
+  }
+
+  displayList(): ITable[] {
+    return this.tableData.slice(this.start, this.end);
+  }
+
+  range(start: number, end: number): number[] {
+    return Array.from({ length: end - start + 1 }, (_, i) => i + start);
+  }
+
+  decrementPage(): void {
+    if (this.currentPage > 1) this.changePage(this.currentPage - 1);
+  }
+
+  incrementPage(): void {
+    if (this.currentPage < this.pagesCount)
+      this.changePage(this.currentPage + 1);
+  }
+
+  updatePages(): void {
+    const maxPagesToShow = 5;
+    const maxPagesBeforeCurrentPage = Math.floor(maxPagesToShow / 2);
+    const maxPagesAfterCurrentPage = Math.ceil(maxPagesToShow / 2) - 1;
+    let startPage, endPage;
+
+    if (this.pagesCount <= maxPagesToShow) {
+      startPage = 1;
+      endPage = this.pagesCount;
+    } else if (this.currentPage <= maxPagesBeforeCurrentPage) {
+      startPage = 1;
+      endPage = maxPagesToShow;
+    } else if (this.currentPage + maxPagesAfterCurrentPage >= this.pagesCount) {
+      startPage = this.pagesCount - maxPagesToShow + 1;
+      endPage = this.pagesCount;
+    } else {
+      startPage = this.currentPage - maxPagesBeforeCurrentPage;
+      endPage = this.currentPage + maxPagesAfterCurrentPage;
+    }
+
+    this.pages = this.range(startPage, endPage);
+  }
+
+  updateDisplayRange(): void {
     this.start = this.limit * (this.currentPage - 1);
     this.end = this.start + this.limit;
+  }
+
+  trackById(index: number, item: ITable): number | string {
+    return item.id;
+  }
+
+  ngOnInit(): void {
+    this.total = this.tableData.length;
+    this.pagesCount = Math.ceil(this.total / this.limit);
+    this.updatePages();
+    this.updateDisplayRange();
+  }
+
+  ngAfterViewInit(): void {
+    initFlowbite();
   }
 
   ngOnChanges(changes: SimpleChanges): void {
@@ -41,61 +104,7 @@ export class TableComponent implements OnInit {
       this.total = this.tableData.length;
       this.pagesCount = Math.ceil(this.total / this.limit);
       this.updatePages();
+      this.updateDisplayRange();
     }
-  }
-
-  // Function to return only a portion of the data to display for the currently selected page.
-  displayList(page: number): ITable[] {
-    const start = this.limit * (page - 1);
-    const end = start + this.limit;
-    return this.tableData.slice(start, end);
-  }
-  // Set a range of pages (i.e., 1-20 pages)
-  range(start: number, end: number): number[] {
-    return [...Array(end - start + 1).keys()].map((el) => el + start);
-  }
-  // Pagination:  Previous Button
-  decrementPage(): void {
-    if (this.currentPage > 1) this.changePage(--this.currentPage);
-  }
-  // Pagination: Next Button
-  incrementPage(): void {
-    if (this.currentPage < this.pagesCount) this.changePage(++this.currentPage);
-  }
-
-  /**
-   * Update the pages array based on the current page and total number of pages
-   *
-   * This function is used to determine which pages to display in the pagination
-   * component. It takes into account the current page and the total number of pages,
-   * and calculates the range of pages to display.
-   */
-  updatePages(): void {
-    const maxPagesToShow = 5;
-    const maxPagesBeforeCurrentPage = Math.floor(maxPagesToShow / 2);
-    const maxPagesAfterCurrentPage = Math.ceil(maxPagesToShow / 2) - 1;
-    let startPage, endPage;
-    // If there are fewer pages than the maximum number to show, show all pages
-    if (this.pagesCount <= maxPagesToShow) {
-      startPage = 1;
-      endPage = this.pagesCount;
-    }
-    // If the current page is one of the first pages, show the first X pages
-    else if (this.currentPage <= maxPagesBeforeCurrentPage) {
-      startPage = 1;
-      endPage = maxPagesToShow;
-    }
-    // If the current page is one of the last pages, show the last X pages
-    else if (this.currentPage + maxPagesAfterCurrentPage >= this.pagesCount) {
-      startPage = this.pagesCount - maxPagesToShow + 1;
-      endPage = this.pagesCount;
-    }
-    // Otherwise, show the current page and the X pages around it
-    else {
-      startPage = this.currentPage - maxPagesBeforeCurrentPage;
-      endPage = this.currentPage + maxPagesAfterCurrentPage;
-    }
-    // Set the pages array to the range of pages to display
-    this.pages = this.range(startPage, endPage);
   }
 }
