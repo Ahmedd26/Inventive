822db52d1ec5514c56a8148f1eb9cab35499389e
MohamedSalahAbdallah <eng.mohamedsalahbakr@gmail.com>
2024-09-28 20:27:34 +0300

    "Updated angular.json and products.component.ts: changed ISuppliers to ISupplier, added analytics false in cli, and reformatted code with minor syntax changes."

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -3,7 +3,7 @@ import { FormsModule, NgForm } from '@angular/forms';
 import { type IProduct } from './products.model';
 import { ProductsService } from './products.service';
 import { NgFor, NgIf } from '@angular/common';
-import { ISuppliers } from '../features/suppliers/suppliers.model';
+import { ISupplier } from '../features/suppliers/suppliers.model';
 import { SuppliersService } from '../features/suppliers/suppliers.service';
 import { CategoriesService } from '../features/categories/categories.service';
 import { ICategory } from '../features/categories/categories.model';
@@ -13,69 +13,70 @@ import { ICategory } from '../features/categories/categories.model';
   standalone: true,
   imports: [FormsModule, NgIf, NgFor],
   templateUrl: './products.component.html',
-  styleUrl: './products.component.css'
+  styleUrl: './products.component.css',
 })
-
 export class ProductsComponent {
-
-  productsArray!: IProduct[]
-  suppliersArray!: ISuppliers[]
-  categoriesArray!: ICategory[]
-  errorTypes = null
+  productsArray!: IProduct[];
+  suppliersArray!: ISupplier[];
+  categoriesArray!: ICategory[];
+  errorTypes = null;
   isLoading = false;
 
-  constructor(private productServ: ProductsService, private supplierServ: SuppliersService, private categoryServ: CategoriesService) { }
+  constructor(
+    private productServ: ProductsService,
+    private supplierServ: SuppliersService,
+    private categoryServ: CategoriesService
+  ) {}
 
   onCreateProduct(productData: IProduct) {
-    console.log(productData)
+    console.log(productData);
     this.isLoading = true;
-    this.productServ.createNewProduct(productData)
-      .subscribe({
-        next: (data) => {
-          this.isLoading = false;
-          this.productsArray.push(data);
-          console.log(data)
-        },
-        error: error => {
-          console.log('alert1')
-          console.log(error)
-          this.errorTypes = error.error.errors
-          console.log('alert2')
-          console.log(this.errorTypes)
-        }
-      })
+    this.productServ.createNewProduct(productData).subscribe({
+      next: (data) => {
+        this.isLoading = false;
+        this.productsArray.push(data);
+        console.log(data);
+      },
+      error: (error) => {
+        console.log('alert1');
+        console.log(error);
+        this.errorTypes = error.error.errors;
+        console.log('alert2');
+        console.log(this.errorTypes);
+      },
+    });
   }
 
   onDeleteProduct(proId: any) {
-    console.log(proId)
+    console.log(proId);
     this.productServ.delete(proId).subscribe({
       next: () => {
-        console.log(proId, "deleted")
+        console.log(proId, 'deleted');
       },
-      error: error => {
-        console.log(error)
-      }
-    })
-    console.log(this.productsArray.length)
-    this.productsArray = this.productsArray.filter((element) => element.id !== proId)
-    console.log(this.productsArray.length)
+      error: (error) => {
+        console.log(error);
+      },
+    });
+    console.log(this.productsArray.length);
+    this.productsArray = this.productsArray.filter(
+      (element) => element.id !== proId
+    );
+    console.log(this.productsArray.length);
   }
 
   ngOnInit() {
     this.productServ.getAllProducts().subscribe((data) => {
-      this.productsArray = data
-      console.log("Products array length is", this.productsArray.length)
-    })
-    this.supplierServ.getAll().subscribe(supData => {
-      this.suppliersArray = supData
+      this.productsArray = data;
+      console.log('Products array length is', this.productsArray.length);
+    });
+    this.supplierServ.getAll().subscribe((supData) => {
+      this.suppliersArray = supData;
       // console.log(this.suppliersArray)
-    })
-    this.categoryServ.getAllCategories().subscribe(catData => {
-      this.categoriesArray = catData
+    });
+    this.categoryServ.getAllCategories().subscribe((catData) => {
+      this.categoriesArray = catData;
       // console.log(catData)
       // console.log(this.categoriesArray)
-    })
+    });
   }
-
 }
-
