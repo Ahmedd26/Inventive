97226386c2b64da02feb0f74d50f2ee99960ff79
Ahmedd26 <ahmedabdellatife1@gmail.com>
2024-09-28 21:36:55 +0300

    added scroll links to navbar

--- src/app/shared/landing-page/components/navbar/navbar.component.ts
+++ src/app/shared/landing-page/components/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { ToggleDarkLightComponent } from '../../../components/top-bar/components/toggle-dark-light/toggle-dark-light.component';
+import { ScrollService } from '../../../../core/services/scroll-service/scroll.service';
 
 @Component({
   selector: 'app-navbar',
@@ -8,4 +9,10 @@ import { ToggleDarkLightComponent } from '../../../components/top-bar/components
   imports: [RouterLink, ToggleDarkLightComponent],
   templateUrl: './navbar.component.html',
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  constructor(private scrollService: ScrollService) {}
+
+  scrollToSection(section: string) {
+    this.scrollService.scrollToFragment(section);
+  }
+}
