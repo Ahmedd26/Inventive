b99c7ca5709dec6589cc11680441d87cf684b338
Ahmedd26 <ahmedabdellatife1@gmail.com>
2024-10-03 23:02:03 +0300

    "Updated product-adjustment component to display user names instead of IDs, added user service to fetch user data"

--- src/app/features/products/components/show-product/components/product-adjustment/product-adjustment.component.ts
+++ src/app/features/products/components/show-product/components/product-adjustment/product-adjustment.component.ts
@@ -1,6 +1,9 @@
-import { Component, Input } from '@angular/core';
+import { UsersService } from './../../../../../users/users.service';
+import { Component, Input, OnInit } from '@angular/core';
 import { type IAdjustment } from '../../../../products.model';
 import { CommonModule } from '@angular/common';
+import { IUser } from '../../../../../users/users.model';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-product-adjustment',
@@ -8,6 +11,28 @@ import { CommonModule } from '@angular/common';
   imports: [CommonModule],
   templateUrl: './product-adjustment.component.html',
 })
-export class ProductAdjustmentComponent {
+export class ProductAdjustmentComponent implements OnInit {
   @Input({ required: true }) adjustments!: IAdjustment[];
+  users!: IUser[];
+  usersError: any;
+  constructor(private usersService: UsersService) {}
+
+  getUserName(userId: number): string {
+    if (this.users) {
+      const user = this.users.find((u) => u.id === userId);
+      return user ? user.name : 'Unknown';
+    }
+    return 'Unknown user';
+  }
+
+  ngOnInit(): void {
+    this.usersService.getAll().subscribe({
+      next: (users) => {
+        this.users = users;
+      },
+      error: (error: HttpErrorResponse) => {
+        this.usersError = error.error.errors;
+      },
+    });
+  }
 }
