b714660706be02d77db816bb063e7144e4424ca4
ahmedadelattya <ahmedadelattya2@gmail.com>
2024-10-05 03:43:20 +0300

    editng the suppliers component and model and service and main page

--- src/app/features/suppliers/suppliers.model.ts
+++ src/app/features/suppliers/suppliers.model.ts
@@ -8,3 +8,10 @@ export interface ISupplier {
   created_at?: string;
   updated_at?: string;
 }
+export interface ISupplierError {
+  name?: string[];
+  email?: string[];
+  phone?: string[];
+  address?: string[];
+  image?: string[];
+}
