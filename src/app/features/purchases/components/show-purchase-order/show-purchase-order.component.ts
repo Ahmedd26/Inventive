f8c68a007c4394ddcecba074dd90d4034309efe2
Ahmedd26 <ahmedabdellatife1@gmail.com>
2024-10-09 19:05:26 +0300

    "Updated show-purchase-order component HTML and TS files to display purchase order details"

--- src/app/features/purchases/components/show-purchase-order/show-purchase-order.component.ts
+++ src/app/features/purchases/components/show-purchase-order/show-purchase-order.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { IconsModule } from '../../../../shared/icons/icons.module';
+import { PurchasesService } from './../../purchases.service';
+import { LoadingComponent } from '../../../../shared/components/loading/loading.component';
+import {
+  type IProductPurchaseOrder,
+  type IPurchase,
+} from '../../purchases.model';
 
 @Component({
   selector: 'app-show-purchase-order',
   standalone: true,
-  imports: [IconsModule],
+  imports: [IconsModule, LoadingComponent, RouterLink, CommonModule],
   templateUrl: './show-purchase-order.component.html',
 })
-export class ShowPurchaseOrderComponent {}
+export class ShowPurchaseOrderComponent implements OnInit {
+  isLoading = false;
+  purchase!: IPurchase;
+  constructor(
+    private purchasesService: PurchasesService,
+    private activatedRoute: ActivatedRoute,
+  ) {}
+
+  ngOnInit() {
+    this.isLoading = true;
+    const routeId = this.activatedRoute.snapshot.params['id'];
+    this.purchasesService.get(+routeId).subscribe({
+      next: (purchase: IPurchase) => {
+        this.purchase = purchase;
+        this.isLoading = false;
+        console.log(purchase);
+      },
+      error: (error: any) => {
+        console.log(error);
+        this.isLoading = false;
+      },
+    });
+  }
+}
