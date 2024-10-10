8e0e50eb6218d7cd35039e2cc0174657bc436c56
Ahmedd26 <ahmedabdellatife1@gmail.com>
2024-10-10 22:38:15 +0300

    "Refactored SalesComponent: removed form creation and editing, added pagination, and updated template to display sales orders in a table"

--- src/app/features/sales/sales.component.ts
+++ src/app/features/sales/sales.component.ts
@@ -1,98 +1,80 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SalesService } from './sales.service';
 import { FormsModule } from '@angular/forms';
-import { NgFor, NgIf } from '@angular/common';
+import { CommonModule, NgFor, NgIf } from '@angular/common';
 import { ISalesOrder } from './sales.model';
-import { IUser } from '../users/users.model';
+import { HttpErrorResponse } from '@angular/common/http';
+import { LoadingComponent } from '../../shared/components/loading/loading.component';
+import { RouterLink } from '@angular/router';
+import { IconsModule } from '../../shared/icons/icons.module';
+import { PaginationComponent } from '../../shared/components/pagination/pagination.component';
+import { InvoicesService } from '../../core/services/invoices-service/invoices.service';
 
 @Component({
   selector: 'app-sales',
   standalone: true,
-  imports: [FormsModule, NgIf, NgFor],
+  imports: [
+    CommonModule,
+    RouterLink,
+    LoadingComponent,
+    IconsModule,
+    PaginationComponent,
+  ],
   templateUrl: './sales.component.html',
 })
-export class SalesComponent {
-  amountPattern = '^[1-9][0-9]*$';
-  updateAmountPattern = '^[1-9][0-9]*$';
-  salesArray!: ISalesOrder[];
-  usersArray!: IUser[];
-  APIerrors = null;
-  updateAPIerrors = null;
+export class SalesComponent implements OnInit {
+  isLoading = false;
+  sales!: ISalesOrder[];
+  errors: any = {};
+  constructor(
+    private salesService: SalesService,
+    private invoicesService: InvoicesService,
+  ) {}
 
-  constructor(private salesServ: SalesService) {}
-
-  onCreateSales(formData: ISalesOrder) {
-    this.salesServ.create(formData).subscribe({
-      next: (data) => {
-        // console.log(data)
-        console.log('Added Successfully');
-        this.APIerrors = null;
-        this.salesArray.push(data);
-      },
-      error: (errorRes) => {
-        this.APIerrors = errorRes.error.errors;
-        console.log(errorRes);
-      },
-    });
+  //** ---------------------- START PAGINATION -------------------------- **//
+  paginatedSales: ISalesOrder[] = [];
+  totalItems: number = 0;
+  itemsPerPage: number = 10;
+  updatePaginatedSales(page: number) {
+    const startIndex = (page - 1) * this.itemsPerPage;
+    const endIndex = startIndex + this.itemsPerPage;
+    this.paginatedSales = this.sales.slice(startIndex, endIndex);
   }
 
-  onDeleteSales(salesId: number) {
-    this.salesServ.delete(salesId).subscribe({
-      next: (data) => {
-        // console.log(data)
-        console.log('DELETED Successfully');
-        this.salesArray = this.salesArray.filter(
-          (element) => element.id !== salesId,
-        );
-      },
-      error: (errorRes) => {
-        // console.log(errorRes)
-      },
-    });
+  onPageChange(page: number) {
+    this.updatePaginatedSales(page);
+  }
+  //** ---------------------- END PAGINATION -------------------------- **//
+
+  calculateTotalQuantity(salesOrder: ISalesOrder): number {
+    return salesOrder.product_sales_orders.reduce(
+      (total, productOrder) => total + productOrder.quantity,
+      0,
+    );
   }
 
-  onUpdateSales(form: ISalesOrder, salesId: number) {
-    this.salesServ.update(form, salesId).subscribe({
-      next: (data) => {
-        // console.log(data)
-        console.log('updated successfully');
-        this.updateAPIerrors = null;
-        this.salesArray = this.salesArray.map((element) => {
-          if (element.id === salesId) {
-            return {
-              ...element,
-              total_amount: form.total_amount,
-              user_id: form.user_id,
-            };
-          }
-          return element;
-        });
-      },
-      error: (errorRes) => {
-        console.log(errorRes);
-        this.updateAPIerrors = errorRes.error.errors;
-      },
-    });
+  getSaleInvoice(id: number) {
+    this.invoicesService.getSale(id);
   }
 
   ngOnInit() {
-    this.salesServ.getAll().subscribe({
-      next: (data) => {
-        // console.log(data)
-        this.salesArray = data;
+    this.isLoading = true;
+    this.salesService.getAll().subscribe({
+      next: (sales) => {
+        this.isLoading = false;
+        this.sales = sales;
+        // ** ---------- PAGINATION ---------- **//
+        this.totalItems = this.sales.length;
+        this.updatePaginatedSales(1);
       },
-      error: (errorRes) => {
-        console.log(errorRes);
+      error: (error: HttpErrorResponse) => {
+        this.isLoading = false;
+        console.log(error.message);
+        this.errors = {
+          status: error.status,
+          message: error.message,
+        };
       },
     });
-    // this.salesServ.getAllUsers().subscribe({
-    //   next: usersData => {
-    //     // console.log(usersData)
-    //     this.usersArray = usersData
-    //   },
-    //   error: errorRes => {
-    //     console.log(errorRes)
-    //   }
-    // })
   }
 }
