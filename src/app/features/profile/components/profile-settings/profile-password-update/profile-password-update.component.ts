acf257e9d6e29a96d4c015c4e8150695d5b002c8
Ahmedd26 <ahmedabdellatife1@gmail.com>
2024-10-12 02:23:08 +0300

    "Updated profile password update component with form validation, error handling, and API call"

--- src/app/features/profile/components/profile-settings/profile-password-update/profile-password-update.component.ts
+++ src/app/features/profile/components/profile-settings/profile-password-update/profile-password-update.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { IUser } from '../../../../users/users.model';
+import { UsersService } from '../../../../users/users.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-profile-password-update',
@@ -7,6 +10,41 @@ import { FormsModule } from '@angular/forms';
   imports: [FormsModule],
   templateUrl: './profile-password-update.component.html',
 })
-export class ProfilePasswordUpdateComponent {
-  passwordForm(data: any) {}
+export class ProfilePasswordUpdateComponent implements OnInit {
+  user!: IUser;
+  current_passwordError = '';
+  new_passwordError = '';
+  new_password_confirmationError = '';
+
+  constructor(private usersService: UsersService) {}
+
+  passwordForm(form: NgForm) {
+    console.log(form.valid);
+
+    if (form.valid) {
+      this.usersService.update(form.value, this.user.id).subscribe({
+        next: (res) => {
+          console.log(res);
+          const currentUserData = JSON.parse(localStorage.getItem('userData')!);
+          currentUserData.user.name = res.name;
+          localStorage.setItem('userData', JSON.stringify(currentUserData));
+        },
+        error: (error: HttpErrorResponse) => {
+          if (error?.error?.error?.current_password) {
+            this.current_passwordError = error.error.error.current_password;
+            console.log(error.error.error.current_password);
+          }
+          if (error?.error?.message) {
+            this.new_password_confirmationError = error.error.message;
+            console.log(error.error.message);
+          }
+          // P@ssw0rD
+        },
+      });
+    }
+  }
+
+  ngOnInit(): void {
+    this.user = JSON.parse(localStorage.getItem('userData')!)?.user;
+  }
 }
