df40a9ca5f3499c68f49e1f9ce2fad9718e72d79
Ahmedd26 <ahmedabdellatife1@gmail.com>
2024-10-11 04:48:47 +0300

    "Added pagination to supplier products component"

--- src/app/features/suppliers/components/show-supplier/supplier-products/supplier-products.component.ts
+++ src/app/features/suppliers/components/show-supplier/supplier-products/supplier-products.component.ts
@@ -1,15 +1,35 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IProduct } from '../../../../products/products.model';
 import { IconsModule } from '../../../../../shared/icons/icons.module';
 import { RouterLink } from '@angular/router';
+import { PaginationComponent } from '../../../../../shared/components/pagination/pagination.component';
 
 @Component({
   selector: 'app-supplier-products',
   standalone: true,
-  imports: [CommonModule,IconsModule,RouterLink],
+  imports: [CommonModule, IconsModule, RouterLink, PaginationComponent],
   templateUrl: './supplier-products.component.html',
 })
-export class SupplierProductsComponent {
+export class SupplierProductsComponent implements OnInit {
   @Input({ required: true }) products!: IProduct[];
+  //** ---------------------- START PAGINATION -------------------------- **//
+  paginatedProduct: IProduct[] = [];
+  totalItems: number = 0;
+  itemsPerPage: number = 4;
+  updatePaginatedProducts(page: number) {
+    const startIndex = (page - 1) * this.itemsPerPage;
+    const endIndex = startIndex + this.itemsPerPage;
+    this.paginatedProduct = this.products.slice(startIndex, endIndex);
+  }
+
+  onPageChange(page: number) {
+    this.updatePaginatedProducts(page);
+  }
+  //** ---------------------- END PAGINATION -------------------------- **//
+  ngOnInit() {
+    // ** ---------- PAGINATION ---------- **//
+    this.totalItems = this.products.length;
+    this.updatePaginatedProducts(1);
+  }
 }
