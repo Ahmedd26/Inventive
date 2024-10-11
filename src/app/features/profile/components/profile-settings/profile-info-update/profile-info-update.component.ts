8d136b1a5ab8d154b918d6f26b83f1ee10d03ca4
Ahmedd26 <ahmedabdellatife1@gmail.com>
2024-10-12 01:10:38 +0300

    "Updated profile info update component with form validation and submission logic"

--- src/app/features/profile/components/profile-settings/profile-info-update/profile-info-update.component.ts
+++ src/app/features/profile/components/profile-settings/profile-info-update/profile-info-update.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { UsersService } from './../../../../users/users.service';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { IUser } from '../../../../users/users.model';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-profile-info-update',
@@ -7,6 +10,28 @@ import { FormsModule } from '@angular/forms';
   imports: [FormsModule],
   templateUrl: './profile-info-update.component.html',
 })
-export class ProfileInfoUpdateComponent {
-  profileInfoForm(data: any) {}
+export class ProfileInfoUpdateComponent implements OnInit {
+  user!: IUser;
+
+  constructor(private usersService: UsersService) {}
+
+  profileInfoForm(form: NgForm) {
+    if (form.valid) {
+      this.usersService.update(form.value, this.user.id).subscribe({
+        next: (res) => {
+          console.log(res);
+          const currentUserData = JSON.parse(localStorage.getItem('userData')!);
+          currentUserData.user.name = res.name;
+          localStorage.setItem('userData', JSON.stringify(currentUserData));
+        },
+        error: (error: HttpErrorResponse) => {
+          console.error(error.error.message);
+        },
+      });
+    }
+  }
+
+  ngOnInit(): void {
+    this.user = JSON.parse(localStorage.getItem('userData')!)?.user;
+  }
 }
