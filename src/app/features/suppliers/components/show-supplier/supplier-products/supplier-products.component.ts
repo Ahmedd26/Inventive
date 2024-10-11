be95b86df3e931444e390fd28507b1265af237c4
Ahmedd26 <ahmedabdellatife1@gmail.com>
2024-10-11 04:37:28 +0300

    "Updated supplier-products component HTML and TS files: added sku and current stock columns, modified product ID column to include link, and updated imports in TS file."

--- src/app/features/suppliers/components/show-supplier/supplier-products/supplier-products.component.ts
+++ src/app/features/suppliers/components/show-supplier/supplier-products/supplier-products.component.ts
@@ -1,11 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IProduct } from '../../../../products/products.model';
+import { IconsModule } from '../../../../../shared/icons/icons.module';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-supplier-products',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,IconsModule,RouterLink],
   templateUrl: './supplier-products.component.html',
 })
 export class SupplierProductsComponent {
