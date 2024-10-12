4b3a5f6f907abe4affea582796cb0aaa8e5cdd5a
Ahmedd26 <ahmedabdellatife1@gmail.com>
2024-10-12 03:49:35 +0300

    "Updated top-bar component HTML and TS files with various changes to classes, attributes, and logic"

---
+++ src/app/features/profile/components/latest-transactions/latest-transactions.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-latest-transactions',
+  standalone: true,
+  imports: [],
+  templateUrl: './latest-transactions.component.html',
+})
+export class LatestTransactionsComponent {}
--- src/app/shared/components/top-bar/top-bar.component.ts
+++ src/app/shared/components/top-bar/top-bar.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ToggleDarkLightComponent } from './components/toggle-dark-light/toggle-dark-light.component';
 import { LogoutComponent } from './components/logout/logout.component';
+import { IUser } from '../../../features/users/users.model';
+import { RouterLink } from '@angular/router';
+import { AuthService } from '../../../core/auth/services/auth.service';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-top-bar',
   standalone: true,
-  imports: [ToggleDarkLightComponent, LogoutComponent],
+  imports: [ToggleDarkLightComponent, LogoutComponent, RouterLink],
   templateUrl: './top-bar.component.html',
 })
-export class TopBarComponent {}
+export class TopBarComponent implements OnInit {
+  user!: IUser;
+
+  constructor(private authService: AuthService) {}
+  logout(): void {
+    this.authService.logout();
+  }
+
+  ngOnInit(): void {
+    initFlowbite();
+    this.user = JSON.parse(localStorage.getItem('userData')!)?.user;
+  }
+}
