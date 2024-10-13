bfb768bb80e206cb477410728ed8eb26a05b8486
Ahmedd26 <ahmedabdellatife1@gmail.com>
2024-10-14 02:50:42 +0300

    "Removed Primeng accordion component and added Chart.js chart component to dashboard component, updated styles and package dependencies accordingly."

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,10 +1,88 @@
 import { Component } from '@angular/core';
-import { AccordionModule } from 'primeng/accordion';
+import { ChartModule } from 'primeng/chart';
+
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [AccordionModule],
+  imports: [ChartModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+  basicData: any;
+
+  basicOptions: any;
+
+  ngOnInit() {
+    const primaryElement = document.getElementById(
+      'primaryText',
+    ) as HTMLElement;
+    const secondaryElement = document.getElementById(
+      'secondaryText',
+    ) as HTMLElement;
+
+    const textColor = getComputedStyle(primaryElement).color;
+    const textColorSecondary = getComputedStyle(secondaryElement).color;
+
+    const documentStyle = getComputedStyle(document.documentElement);
+    // const textColor = documentStyle.getPropertyValue('--text-color');
+    // const textColorSecondary = documentStyle.getPropertyValue(
+    //   '--text-color-secondary',
+    // );
+    const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
+
+    this.basicData = {
+      labels: ['Q1', 'Q2', 'Q3', 'Q4'],
+      datasets: [
+        {
+          label: 'Sales',
+          data: [540, 325, 702, 620],
+          backgroundColor: [
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+          ],
+          borderColor: [
+            'rgb(255, 159, 64)',
+            'rgb(75, 192, 192)',
+            'rgb(54, 162, 235)',
+            'rgb(153, 102, 255)',
+          ],
+          borderWidth: 1,
+        },
+      ],
+    };
+
+    this.basicOptions = {
+      plugins: {
+        legend: {
+          labels: {
+            color: textColor,
+          },
+        },
+      },
+      scales: {
+        y: {
+          beginAtZero: true,
+          ticks: {
+            color: textColorSecondary,
+          },
+          grid: {
+            color: surfaceBorder,
+            drawBorder: false,
+          },
+        },
+        x: {
+          ticks: {
+            color: textColorSecondary,
+          },
+          grid: {
+            color: surfaceBorder,
+            drawBorder: false,
+          },
+        },
+      },
+    };
+  }
+}
